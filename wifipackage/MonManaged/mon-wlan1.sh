eb171a31dc5d3069dd4781e74441202fe420948d
CybernetiX-S3C <john.p.modica@gmail.com>
2018-06-22 23:22:29 -0400

    New Repos

---
+++ wifipackage/MonManaged/mon-wlan1.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+cat << "EOF"
+
+                             --=---+---=--
+                                    \___
+                              ---=---+-I-=---
+                                     |\
+                             ---=----+----=---
+                                     |
+                                     ^
+
+
+                    "+-------------------------------+"
+                    "---------------------------------"
+                    "----     Mon Mode Script     ----"
+                    "----       Created by        ----"
+                    "----     John Poli Modica    ----"
+                    "----  CEO/Cyber Security Pro ----"
+                    "---- Modica Design Solutions ----"
+                    "----	    &             ----"
+                    "----       CybernetiX        ----"
+                    "---------------------------------"
+                    "+-------------------------------+"
+
+
+
+
+EOF
+
+echo "Hello, Stranger! What a great day we are having."
+echo "I see you want to change into Monitor mode on your WiFi card."
+echo "Let us see what we can do! Hang tight!"
+sleep 15
+
+echo "Searching for Wifi card..."
+sleep 10
+
+echo "wlan1 found."
+sleep 5
+
+echo "Lets first take down wlan1"
+ifconfig wlan1 down
+sleep 10
+
+echo "Now we have to change wlan1 to monitor."
+sudo iwconfig wlan1 mode monitor
+sleep 10
+
+echo "Now we place your card back up."
+ifconfig wlan1 up
+sleep 10
+
+echo "Checking to make sure everything is ok."
+iwconfig
+sleep 15
+
+echo "Monitor Mode Set! Congrats! When using this it will still be wlan1 and (NOT) wlan1mon."
+cat << "EOF"
+
+                   --------------------------------
+                   --------------------------------
+                             ...----....
+                         ..-:"''         ''"-..
+                      .-'                      '-.
+                    .'              .     .       '.
+                  .'   .          .    .      .    .''.
+                .'  .    .       .   .   .     .   . ..:.
+              .' .   . .  .       .   .   ..  .   . ....::.
+             ..   .   .      .  .    .     .  ..  . ....:IA.
+            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+                     "V::... . .I::IHHMMV"'
+                       '"VHVHHHAHHHHMMV:"'
+
+
+EOF
+
+
---
+++ wifipackage/README.md
@@ -0,0 +1,44 @@
+Author: John Modica @ CybernetiX S3C
+
+To start the scripts just use one of the following: 
+bash nameoffile.sh
+sh nameoffile.sh
+./nameoffile.sh
+
+cat << "EOF"            
+             __________
+            /'        /|
+           /         / |_
+          /         /  //|
+         /_________/  ////|
+        |   _ _    | 8o////|
+        | /'// )_  |   8///|
+        |/ // // ) |   8o///|
+        / // // //,|  /  8//|
+       / // // /// | /   8//|
+      / // // ///__|/    8//|
+     /.(_)// /// |       8///|
+    (_)' `(_)//| |       8////|___________
+   (_) /_\ (_)'| |        8///////////////
+   (_) \"/ (_)'|_|         8/////////////
+    (_)._.(_) d' Hb         8oooooooopb'
+      `(_)'  d'  H`b
+            d'   `b`b
+           d'     H `b
+          d'      `b `b
+         d'           `b
+        d'             `b
+EOF
+WARNING!! 
+Changing the TxPower is and can be dangerous and also illegal 
+in differrent countries!
+Changing any higher can cause radiation poisoning, cancer,
+and can burn your card out!
+
+https://www.cisco.com/c/en/us/support/docs/dial-access/asynchronous-connections/15380-trans-rec-15380.html
+
+https://www.snbforums.com/threads/dangerous-to-rise-tx-power.19240/
+
+https://forums.kali.org/showthread.php?28809-Raising-TX-Power-What-Does-This-Really-Do
+
+# WifiPackage
